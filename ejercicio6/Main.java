b995488273229b6d62e3439a408af5bb59fd5d53
jpabloallin <jpac.647@gmail.com>
2022-02-18 18:47:27 -0500

    Creación y solución ejercicio 6

---
+++ ejercicio6/Main.java
@@ -0,0 +1,13 @@
+package ejercicio6;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Menu menu = new Menu();
+        menu.receiveNumbers();
+
+}
+}
---
+++ ejercicio6/Menu.java
@@ -0,0 +1,30 @@
+package ejercicio6;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Menu {
+    
+    Vector<Integer> vector = new Vector<Integer>();
+    Integer previous = null;
+
+    public void receiveNumbers() {
+        
+        try (Scanner entry = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("\nIngresa un número: \n");
+                int number = entry.nextInt();
+    
+                if (previous != null && previous == number)
+                    break;
+    
+                vector.add(number);
+                previous = number;
+            }
+        }
+        System.out.println("vector: " + vector);
+    }
+}
+
+
